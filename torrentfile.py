a4d14688451e2bf983bb64b028212db571a8e284
Franco Ayala <frma98@gmail.com>
2022-01-04 17:20:52 -0300

    Cree una funcion para solicitar los pares http

--- torrentfile.py
+++ torrentfile.py
@@ -1,14 +1,16 @@
 from hashlib import sha1
-from requests import get
+from requests import get, Timeout
 from pprint import pprint
 from math import ceil
 from time import sleep
 import bencodepy
 import socket
-
+#from urllib import urlparse
 import requests
+import logging
 
 id = "-BC0012-3456abcde123"
+local_addr = ("172.28.240.197", 55557)
 
 class TorrentFile():
 
@@ -21,21 +23,13 @@ class TorrentFile():
             info_index = buffer.find(b"4:infod")
             self.info_hash = sha1(buffer[info_index+6:-1]).digest()
             self.downloading_file = open("./file", "wb")
-        # request peers from the tracker
-        get_params = {
-            "info_hash": self.info_hash,
-            "peer_id": bytes(id, "utf-8"),
-            "port": 55556,
-            "downloaded": 0,
-            "uploaded": 0,
-            "left": self.meta_data[b"info"][b"length"]
-        }
-        self.peers = bencodepy.bdecode(get(self.meta_data[b"announce"], get_params).content)
+        self.peers = self.request_peers()
         for peer in self.peers[b"peers"]:
             peer[b"am_choking"] = 1
             peer[b"am_interested"] = 0
             peer[b"peer_choking"] = 1
             peer[b"peer_interested"] = 0
+        print(self.peers)
 
     def share(self):
         handshake = (19).to_bytes(1, "big") + b"BitTorrent protocol\0\0\0\0\0\0\0\0"+self.info_hash+bytes(id, "utf-8")
@@ -60,22 +54,40 @@ class TorrentFile():
             self.downloading_file.write(ans[12:])
         self.downloading_file.close()
 
+    def request_peers(self):
+        trackers_list = []
+        peers = []
+        trackers_list.append(self.meta_data[b"announce"])
+        try:
+            trackers_list = [tracker.pop() for tracker in self.meta_data[b"announce-list"]]
+        except KeyError as err:
+            logging.info(str(err.args[0], "utf-8") + "not found in meta_data")
+        
+        for tracker in trackers_list:
+            if b"udp" in tracker:
+                self.request_peers_udp()
+            else:
+                peers = self.request_peers_http()
+        return peers
+
+    def request_peers_udp(self):
+        
+
+    def request_peers_http(self):
+        get_params = {
+            "info_hash": self.info_hash,
+            "peer_id": bytes(id, "utf-8"),
+            "port": local_addr[1],
+            "downloaded": 0,
+            "uploaded": 0,
+            "left": self.meta_data[b"info"][b"length"]
+        }
+        try:
+            return bencodepy.bdecode(get(self.meta_data[b"announce"], get_params, timeout=0.5).content)
+        except Timeout:
+            logging.info(str(self.meta_data[b"announce"], "utf-8") + "timed out")
+
 
 if __name__ == "__main__":
-    a = TorrentFile("./Introducing Data Science - Big Data, Machine Learning and more, using Python tools (2016).pdf Gooner-[rarbg.to].torrent")
-    a.share()
-    """id = "-ZK0012-3456abcde123"
-    handshake = b"19BitTorrent protocol\0\0\0\0\0\0\0\0"+a.info_hash+bytes(id, "UTF-8")
-    handshake = (19).to_bytes(1, "big") + b"BitTorrent protocol\0\0\0\0\0\0\0\0"+a.info_hash+bytes(id, "utf-8")
-    unchoke = (1).to_bytes(1, "big") + (1).to_bytes(1, "big")
-    interested = (1).to_bytes(1, "big") + (2).to_bytes(1, "big")
-    request = (13).to_bytes(1, "big") + (6).to_bytes(1, "big") + (255).to_bytes(1, "big") + (0).to_bytes(1, "big") + (16).to_bytes(1, "big")
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(("172.21.184.95", 55556))
-    s.connect((a.peers[b"peers"][0][b"ip"], a.peers[b"peers"][0][b"port"]))
-    s.send(handshake)
-    print(s.recv(100))
-    s.send(interested)
-    s.send(request)
-    asd = s.recv(100)
-    print(asd)"""
+    a = TorrentFile("Introducing Data Science - Big Data, Machine Learning and more, using Python tools (2016).pdf Gooner-[rarbg.to].torrent")
+    # a.share()
