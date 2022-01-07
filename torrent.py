5c33414be503311c68b5d635f52d9a8b8826bd79
Franco Ayala <frma98@gmail.com>
2022-01-07 15:34:13 -0300

    La clase Torrent ahora usa la clase Peer

--- torrent.py
+++ torrent.py
@@ -1,18 +1,16 @@
-from requests.api import request
 from torrentfile import TorrentMetaData
-from trackers import Tracker, TrackerError
+from trackers import Peer, TrackerError
 from pprint import pprint
-
-client_data = ("-BC0012-3456abcde123", ("172.24.112.129", 56056))
+import client_data
 
 class PeersNotFound(Exception):
     def __init__(self, message):
         self.message = message
 
 class Torrent():
-    def __init__(self, file_path: str, client_data: tuple):
+    def __init__(self, file_path: str):
         self.torrent_meta_data = TorrentMetaData(file_path)
-        self.client_data = client_data
+        # self.client_data = client_data
         self.downloaded = 0
         self.uploaded = 0
         self.left = self.torrent_meta_data.info["length"]
@@ -23,26 +21,28 @@ class Torrent():
         pass
 
     def request_peers(self):
+        peers = []
         request_data = {
             "info_hash": self.torrent_meta_data.info_hash,
-            "peer_id": self.client_data[0],
+            "peer_id": client_data.client_id,
             "downloaded": self.downloaded,
             "left": self.left,
             "uploaded": self.uploaded,
             "event": 0,
-            "ip": self.client_data[1][0],
+            "ip": client_data.ip,
             "key": 0,
             "numwant": -1,
-            "port": self.client_data[1][1]
-        }
+            "port": client_data.port}
+
         trackers = self.torrent_meta_data.trackers
         for tracker in trackers:
             try:
-                self.peers += tracker.request_peers(request_data)
+                peers += tracker.request_peers(request_data)
+                self.peers += [Peer(peer) for peer in peers]
             except TrackerError as err:
-                print("El tracker no respondio. " + err.message)
+                pass
+                # print("El tracker no respondio. " + err.message)
 
 if __name__ == "__main__":
-    torrent = Torrent("./The Complete Chess Course - From Beginning to Winning Chess - 21st Century Edition (2016).epub Gooner-[rarbg.to].torrent",
-                        client_data)
-    pprint(torrent.peers)
+    torrent = Torrent("./The Complete Chess Course - From Beginning to Winning Chess - 21st Century Edition (2016).epub Gooner-[rarbg.to].torrent")
+    pprint(torrent.peers[0].ip)
