f6392f9392b55940f6d8de37b92c9bd608057a4d
Franco Ayala <frma98@gmail.com>
2022-01-07 11:24:03 -0300

    Muchos cambios pequeños
    Saque la funcion get_connection() porque no funcionaba, arregle el nombre de la funcion request_peers() de HTTPTracker, saque la verificacion del transaction id, cree una funcion para crear la solicitud de announce y otras cosas.

--- trackers.py
+++ trackers.py
@@ -21,7 +21,7 @@ class Peer():
     def share(self):
         pass
 
-class Tracker():
+class Tracker:
     
     def __init__(self, url: str):
         url_parsed = urlparse(url)
@@ -34,23 +34,36 @@ class Tracker():
 class UDPTracker(Tracker):
     
     def __init__(self, url: str):
-        super().__init__(url)
+        #super().__init__(url)
+        url_parsed = urlparse(url)
+        self.name = url_parsed.hostname
+        self.port = url_parsed.port
         self.connection: socket.socket = None
         self.connection_id: bytes = None
 
     def request_peers(self, request_data: dict) -> list:
-        self.connection = self.get_connection((request_data["ip"], request_data["port"]))
-        connection_id = self.send_connection_request()
-        peers = self.send_announce(connection_id, request_data)
+        # self.connection = self.get_connection((request_data["ip"], request_data["port"]))
+        addr = (request_data["ip"], request_data["port"])
+        self.connection_id = self.send_connection_request(addr)
+        peers = self.send_announce(request_data, addr)
 
         return peers
 
-    def send_connection_request(self) -> int:
+    def send_connection_request(self, addr: tuple) -> int:
+        try:
+            tracker_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            tracker_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            tracker_socket.settimeout(1)
+            tracker_socket.bind(addr)
+            tracker_socket.connect((self.name, self.port))
+        except socket.gaierror as err:
+            raise TrackerError(err.strerror)
+
         transaction_id = randint(0, 4294967295)
         connect_request = (0x41727101980).to_bytes(8, "big") + (0).to_bytes(4, "big") + (transaction_id).to_bytes(4, "big")
-        self.connection.send(connect_request)
+        tracker_socket.send(connect_request)
         try:
-            connnect_response = self.connection.recv(150)
+            connnect_response = tracker_socket.recv(150)
         except socket.timeout:
             raise TrackerError("Tracker timed out.")
 
@@ -58,57 +71,70 @@ class UDPTracker(Tracker):
             raise TrackerError("Response smaller than 16 bytes.")
         if int.from_bytes(connnect_response[0:4], "big") != 0:
             raise TrackerError("Action field in response is not connect (0).")
-        if connnect_response[4:8] != transaction_id.to_bytes(4, "big"):
+        if int.from_bytes(connnect_response[4:8], "big") != transaction_id:
             raise TrackerError("The tracker answered with a different transaction id.")
+        
         connection_id = int.from_bytes(connnect_response[8:], "big")
+        tracker_socket.close()
         return connection_id
 
-    def send_announce(self, request_data: dict) -> list:
-        peers = []
-        transaction_id = randint(0, 4294967295).to_bytes(4, "big")
-        announce_request = self.make_announce_request(request_data)
-        self.connection.send(announce_request)
+    def send_announce(self, request_data: dict, addr: tuple) -> list:
         try:
-            response = self.connection.recv(150)
-        except socket.timeout as err:
+            tracker_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            tracker_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            tracker_socket.settimeout(0.5)
+            tracker_socket.bind(addr)
+            tracker_socket.connect((self.name, self.port))
+        except socket.gaierror as err:
             raise TrackerError(err.strerror)
+
+        peers = []
+        transaction_id = randint(0, 4294967295)
+        announce_request = self.make_announce_request(request_data)
+        tracker_socket.send(announce_request)
+        try:
+            response = tracker_socket.recv(150)
+        except socket.timeout:
+            raise TrackerError("Socket timed out")
 
         if len(response) < 20:
             raise TrackerError("Response smaller than 20 bytes.")
         if int.from_bytes(response[0:4], "big") != 1:
             raise TrackerError("Action field in response is not announce (1).")
-        if response[4:8] != transaction_id:
-            raise TrackerError("The tracker answered with a different transaction id.")
-
+        #if int.from_bytes(response[4:8], "big") != transaction_id:
+        #    raise TrackerError("The tracker answered with a different transaction id.")
+        print(int.from_bytes(response[4:8], "big"))
+        print(transaction_id)
         for i in range(20, len(response), 6):
             ip = str(response[i]) + "." + str(response[i+1]) + "." + str(response[i+2]) + "." + str(response[i+3])
             port = int.from_bytes(response[i+4:i+6], "big")
             peers.append({b"ip": ip, b"port": port})
         
+        tracker_socket.close()
         return peers
 
     def make_announce_request(self, request_data: dict) -> bytes:
         transaction_id = randint(0, 4294967295)
         #announce_request = self.connection_id + (1).to_bytes(4, "big") + transaction_id + self.info_hash +  bytes(client_data[0], "utf-8") + (0).to_bytes(8, "big") + self.meta_data[b"info"][b"length"].to_bytes(8, "big") + (0).to_bytes(8, "big") + (2).to_bytes(4, "big") + (0).to_bytes(4, "big") + (0).to_bytes(4, "big") + (-1).to_bytes(4, "big", signed = True) + (client_data[1][1]).to_bytes(2, "big")
         announce_request = bytearray()
-        announce_request += self.connection_id                  
+        announce_request += self.connection_id.to_bytes(8, "big")                  
         announce_request += (1).to_bytes(4, "big") # action field, 4 bytes
         announce_request += (transaction_id).to_bytes(4, "big") 
         announce_request += request_data["info_hash"]           
         announce_request += bytes(request_data["peer_id"], "utf-8") 
         announce_request += request_data["downloaded"].to_bytes(8, "big")
-        announce_request += request_data["left"].to_bytes(8, "big")
+        announce_request += request_data["left"]
         announce_request += request_data["uploaded"].to_bytes(8, "big")
         announce_request += request_data["event"].to_bytes(4, "big")
         announce_request += (0).to_bytes(4, "big")  # ip address field, 4 bytes, 0 default
         announce_request += (0).to_bytes(4, "big")  # key field, 4 bytes
-        announce_request += (-1).to_bytes(4, "big") # num_want field, 4 bytes, -1 default
+        announce_request += (-1).to_bytes(4, "big", signed = True) # num_want field, 4 bytes, -1 default
         announce_request += (request_data["port"]).to_bytes(4, "big")
 
         return announce_request
 
 
-    """ La comento porque ahora no uso, quizas sea necesaria mas adelante.
+    """ La comento porque ahora no la uso, quizas sea necesaria mas adelante.
     def udp_scrape(self, connection_id: int, tracker_socket: socket.socket):
         transaction_id = randint(0, 4294967295)
         udp_request = connection_id + \
@@ -117,21 +143,24 @@ class UDPTracker(Tracker):
         tracker_socket.send(udp_request)
         answer = tracker_socket.recv(150)"""
 
-    def get_connection(self, client_data: tuple) -> socket.socket:
+    def get_connection(self, addr: tuple) -> socket.socket:
         if self.connection == None:
             self.connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             self.connection.settimeout(0.5)
-            self.connection.bind(client_data(1))
+            self.connection.bind(addr)
             try:
-                self.connection.connect((self.hostname, self.port))
+                self.connection.connect((self.name, self.port))
             except socket.gaierror as err:
                 raise TrackerError(err.strerror)
-        else:
-            return self.connection
 
 class HTTPTracker(Tracker):
     
-    def request_peers_http(self, request_data: dict):
+    def __init__(self, url: str):
+        # super().__init__(url)
+        self.url = url
+
+
+    def request_peers(self, request_data: dict) -> list:
         """get_params = {
             "info_hash": torrent_meta_data.info_hash,
             "peer_id": bytes(client_data[0], "utf-8"),
@@ -141,9 +170,9 @@ class HTTPTracker(Tracker):
             "left": torrent_meta_data.info["length"]
         }"""
         try:
-            peers = bencodepy.bdecode(get(self.name, request_data, timeout=0.5).content)
+            peers = bencodepy.bdecode(get(self.url, request_data, timeout=0.5).content)
             return peers[b"peers"]
         except Timeout as err:
             raise TrackerError("HTTP GET request to tracker timed out.")
         except ConnectionError as err:
-            raise TrackerError(err.strerror)
+            raise TrackerError("Failed to establish a new connection when sending a HTTP GET request.")
