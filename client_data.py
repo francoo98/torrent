d62b9ec95966d95a5b724f3d57be7109b48e2e31
Franco Ayala <frma98@gmail.com>
2022-01-10 13:23:35 -0300

    Hice que se obtenga la ip del cliente automaticamente

--- client_data.py
+++ client_data.py
@@ -1,3 +1,10 @@
-ip = "172.25.252.23"
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+
+ip = s.getsockname()[0]
 port = 56056
 client_id = "-BC0012-3456abcde123"
+
+s.close()
