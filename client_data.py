b870ce56449e4cbbdf5ea77342d037112445972a
Franco Ayala <frma98@gmail.com>
2022-01-12 09:32:58 -0300

    Saque la ip del modulo client_data

--- client_data.py
+++ client_data.py
@@ -1,10 +1,2 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("8.8.8.8", 80))
-
-ip = s.getsockname()[0]
-port = 56056
 client_id = "-BC0012-3456abcde123"
-
-s.close()
+port = 56056
