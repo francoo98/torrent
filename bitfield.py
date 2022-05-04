3a2ab8557d93a16b17f4c9963fe6ffaaa9a79ab1
Franco Ayala <franco.a98@outlook.com>
2022-05-04 15:27:08 -0300

    Ahora el atributo data de bitfield es un bytearray

--- bitfield.py
+++ bitfield.py
@@ -4,14 +4,13 @@ class BitField():
 
     def __init__(self, size: int):
         self.size = size
-        self.bits = b"\0" * self.size 
+        self.bits = bytearray(self.size) 
 
     @classmethod
     def from_bytes(cls, data: bytes):
         bitfield = cls(len(data))
-        bitfield.bits = data
+        bitfield.bits = bytearray(data)
         return bitfield
-
 
     def add(self, piece_index):
         byte_index = int(piece_index/8)
@@ -19,20 +18,14 @@ class BitField():
         bit_index_in_byte = 7 - (piece_index - byte_index * 8)
         byte_value = byte_value | (2**bit_index_in_byte)
         try:
-            self.bitfield[byte_index] = byte_value
+            self.bits[byte_index] = byte_value
         except IndexError:
             logging.info("Index out of bounds")
 
-    """def value_of(self, piece_index):
-        byte_index = int(piece_index/8)
+    def __getitem__(self, bit_index: int) -> bool:
+        byte_index = int(bit_index/8)
         byte_value = self.bits[byte_index]
-        bit_index_in_byte = 7 - (piece_index - byte_index * 8)
-        byte_value = byte_value >> bit_index_in_byte"""
-
-    def __getitem__(self, piece_index):
-        byte_index = int(piece_index/8)
-        byte_value = self.bits[byte_index]
-        bit_index_in_byte = 7 - (piece_index - byte_index * 8)
+        bit_index_in_byte = 7 - (bit_index - byte_index * 8)
         byte_value = byte_value >> bit_index_in_byte
         byte_value = byte_value & 0b00000001
         return byte_value
