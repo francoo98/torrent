e93b2ce89a19cd9a245e25f35068f60c5be9a8e7
Franco Ayala <frma98@gmail.com>
2022-05-05 15:42:55 -0300

    Cambios en SingleFileManager
    1. Agregue los hashes de la piezas como atributo. 2. Cambie el modo en el que se abre el archivo a descargar. 3. Agregue la funcion calculate_bitfield.

--- filemanager.py
+++ filemanager.py
@@ -1,3 +1,8 @@
+from hashlib import sha1
+from math import ceil
+from os import path
+from bitfield import BitField
+
 class FileManager():
 
     def __init__(self, info: dict):
@@ -6,16 +11,35 @@ class FileManager():
     def write_piece(self, piece: tuple):
         pass
 
+    def get_piece(self, piece_id: int):
+        pass
+
+    def calculate_bitfield(self):
+        pass
+
 class SingleFileManager(FileManager):
 
     def __init__(self, info: dict):
         self.name = str(info["name"], "UTF-8")
         self.length = info["length"]
         self.piece_len = info["piece length"]
-        self.file = open("./" + self.name, "ab")
+        self.pieces = info["pieces"]
+        self.file = open("./" + self.name, "x+b")
         self.file.seek(0)
 
     def write_piece(self, piece: tuple):
         offset = piece(0)*self.piece_len
         self.file.seek(offset)
         self.file.write(piece(1))
+
+    def calculate_bitfield(self):
+        piece_id = 0
+        bitfield = BitField(ceil(self.length/self.piece_len/8))
+
+        while (piece := self.file.read(self.piece_len)) != b"":
+            phash = sha1(piece).digest()
+            if phash == self.pieces[piece_id]:
+                bitfield.add(piece_id)
+            piece_id += 1
+
+        return bitfield
